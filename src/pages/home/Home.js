1c2e874e1d53f3f201b35c01510571519fe92749
Dinesh Katwal <medineshkatwal@gmail.com>
2018-05-22 22:54:28 +0545

    Added custom google search

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,7 +5,7 @@ import './Search.css';
 
 class Home extends Component {
   render() {
-    console.log(this.props.match.params.value);
+    console.log(this.props);
     return (
       <React.Fragment>
         <Search default="bbc-news" store={this.props.store} value={this.props.match.params.value}/>
